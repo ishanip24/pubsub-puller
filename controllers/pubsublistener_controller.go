4850cfbdab9b15f2c8a576f4e4cab3ac772a69cb
Ishani Pandya <ishanip0424@gmail.com>
2022-09-01 14:28:38 -0700

    generated pubsublistener api

--- controllers/pubsublistener_controller.go
+++ controllers/pubsublistener_controller.go
@@ -24,7 +24,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	webappv1 "example.com/m/api/v1"
+	batchv1 "example.com/m/api/v1"
 )
 
 // PubSubListenerReconciler reconciles a PubSubListener object
@@ -33,9 +33,9 @@ type PubSubListenerReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=webapp.company.org,resources=pubsublisteners,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=webapp.company.org,resources=pubsublisteners/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=webapp.company.org,resources=pubsublisteners/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch.company.org,resources=pubsublisteners,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=batch.company.org,resources=pubsublisteners/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=batch.company.org,resources=pubsublisteners/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -57,6 +57,6 @@ func (r *PubSubListenerReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 // SetupWithManager sets up the controller with the Manager.
 func (r *PubSubListenerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&webappv1.PubSubListener{}).
+		For(&batchv1.PubSubListener{}).
 		Complete(r)
 }
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -31,7 +31,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	webappv1 "example.com/m/api/v1"
+	batchv1 "example.com/m/api/v1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -65,7 +65,7 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
-	err = webappv1.AddToScheme(scheme.Scheme)
+	err = batchv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
